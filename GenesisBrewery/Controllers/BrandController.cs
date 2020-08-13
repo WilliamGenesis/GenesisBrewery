a79d25fee3be790c0f180bc9292ed74bc76b1f92
William Bienfait <wbienfait@genesis-consult.com>
2020-08-13 11:57:38 +0200

    Add - Routes and controllers

---
+++ GenesisBrewery/Controllers/BrandController.cs
@@ -0,0 +1,27 @@
+﻿using BrandDomain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace GenesisBrewery.Controllers
+{
+    public class BrandController : Controller
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateBeer(Beer beer)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteBeer(Guid beerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GenesisBrewery/Controllers/WholesalerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WholesalerDomain;
+
+namespace GenesisBrewery.Controllers
+{
+    public class WholesalerController : Controller
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetWholesalerStock(Guid wholesalerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetWholesalerByItem(Guid itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetWholesalerByItem(StockItem stockItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateStockItem(StockItem stockItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
