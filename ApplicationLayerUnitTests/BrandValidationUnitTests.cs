d3cf4f4166f9826b8904ce9cedab74517ebff82a
William Bienfait <wbienfait@genesis-consult.com>
2020-08-21 10:46:31 +0200

    Add - Brand validation unit tests

---
+++ ApplicationLayerUnitTests/BrandValidationUnitTests.cs
@@ -0,0 +1,122 @@
+﻿using ApplicationLayer.Queries;
+using ApplicationLayer.Validations;
+using BrandDomain;
+using Moq;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApplicationLayerUnitTests
+{
+    public class BrandValidationUnitTests
+    {
+        [Fact]
+        public async Task BeerExists_GivenAnExistingBeerId_ShouldReturnTrue()
+        {
+            //Arrange
+            var guid = Guid.NewGuid();
+
+            var mockedBrandQuery = new Mock<IBrandQuery>();
+            mockedBrandQuery.Setup(query => query.GetBeer(It.IsAny<Guid>()))
+                .ReturnsAsync(new Beer());
+
+            var brandValidation = new BrandValidation(mockedBrandQuery.Object);
+
+            //Act
+            var result = await brandValidation.BeerExists(guid);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task BeerExists_GivenAnNonExistingBeerId_ShouldReturnFalse()
+        {
+            //Arrange
+            var guid = Guid.NewGuid();
+
+            var mockedBrandQuery = new Mock<IBrandQuery>();
+            mockedBrandQuery.Setup(query => query.GetBeer(It.IsAny<Guid>()))
+                .ReturnsAsync((Beer) null);
+
+            var brandValidation = new BrandValidation(mockedBrandQuery.Object);
+
+            //Act
+            var result = await brandValidation.BeerExists(guid);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task ValidateBeer_GivenValidBreweryId_ShouldReturnAllValidationResultsSuccessful()
+        {
+            //Arrange
+            var guid = Guid.NewGuid();
+            var beer = new Beer
+            {
+                BreweryId = guid
+            };
+
+            var mockedBrandQuery = new Mock<IBrandQuery>();
+            mockedBrandQuery.Setup(query => query.GetBrewery(It.IsAny<Guid>()))
+                .ReturnsAsync(new Brewery());
+
+            var brandValidation = new BrandValidation(mockedBrandQuery.Object);
+
+            //Act
+            var results = await brandValidation.ValidateBeer(beer);
+
+            //Assert
+            Assert.True(results.All(result => result == ValidationResult.Success));
+        }
+
+        [Fact]
+        public async Task ValidateBeer_GivenAnInvalidBreweryId_ShouldReturnAValidationResultWithExpectedErrorMessage()
+        {
+            //Arrange
+            var guid = Guid.NewGuid();
+            var beer = new Beer
+            {
+                BreweryId = guid
+            };
+
+            var expectedErrorMessage = "A beer must always be linked to an existing brewery";
+
+            var mockedBrandQuery = new Mock<IBrandQuery>();
+            mockedBrandQuery.Setup(query => query.GetBrewery(It.IsAny<Guid>()))
+                .ReturnsAsync((Brewery) null);
+
+            var brandValidation = new BrandValidation(mockedBrandQuery.Object);
+
+            //Act
+            var results = await brandValidation.ValidateBeer(beer);
+
+            //Assert
+            Assert.Contains(results, result => result.ErrorMessage.Equals(expectedErrorMessage));
+        }
+
+        [Fact]
+        public async Task ValidateBeer_GivenNoBreweryId_ShouldReturnAValidationResultWithExpectedErrorMessage()
+        {
+            //Arrange
+            var guid = Guid.NewGuid();
+            var beer = new Beer
+            {
+                BreweryId = Guid.Empty
+            };
+
+            var expectedErrorMessage = "A beer must always be linked to a brewery";
+
+            var brandValidation = new BrandValidation(null);
+
+            //Act
+            var results = await brandValidation.ValidateBeer(beer);
+
+            //Assert
+            Assert.Contains(results, result => result.ErrorMessage.Equals(expectedErrorMessage));
+        }
+    }
+}
---
+++ ApplicationLayerUnitTests/WholesalerServiceUnitTests.cs
@@ -0,0 +1,104 @@
+﻿using ApplicationLayer.Business;
+using ApplicationLayer.Queries;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using WholesalerDomain;
+using Xunit;
+
+namespace ApplicationLayerUnitTests
+{
+    public class WholesalerServiceUnitTests
+    {
+        [Fact]
+        public async Task GenerateQuote_GivenAQuoteRequestWithLessThan10Items_ShouldReturnARawPriceAndPriceEqualToExpectedPrice()
+        {
+            //Arrange
+            var requestedItemId = Guid.NewGuid();
+
+            var wholesalerStock = new[] {
+                new StockItem{ItemId = requestedItemId, Quantity = 5, UnitPrice = 1.0f}
+            };
+
+            var expectedPrice = 5.0f;
+
+            var quoteRequest = new QuoteRequest { WholesalerId = Guid.NewGuid(),RequestItems = new[] { new RequestItem { ItemId = requestedItemId, Quantity = 5 } } };
+
+            var mockedWholesalerQuery = new Mock<IWholesalerQuery>();
+            mockedWholesalerQuery.Setup(query => query.GetWholesalerStock(It.IsAny<Guid>()))
+                .ReturnsAsync(wholesalerStock);
+
+            var wholesalerService = new WholesalerService(mockedWholesalerQuery.Object, null);
+
+            //Act
+            var result = await wholesalerService.GenerateQuote(quoteRequest);
+
+            //Assert
+            Assert.Equal(expectedPrice, result.Price);
+            Assert.Equal(expectedPrice, result.RawPrice);
+            Assert.Equal(0, result.Discount);
+        }
+
+        [Fact]
+        public async Task GenerateQuote_GivenAQuoteRequestWithMoreThan10Items_ShouldReturnAPriceWith10PorcentDiscountFromRawPrice()
+        {
+            //Arrange
+            var requestedItemId = Guid.NewGuid();
+
+            var wholesalerStock = new[] {
+                new StockItem{ItemId = requestedItemId, Quantity = 15, UnitPrice = 1.0f}
+            };
+
+            var expectedRawPrice = 15.0f;
+            var expectedDiscout = 1.5f;
+            var expectedPrice = 13.5f;
+
+            var quoteRequest = new QuoteRequest { WholesalerId = Guid.NewGuid(), RequestItems = new[] { new RequestItem { ItemId = requestedItemId, Quantity = 15 } } };
+
+            var mockedWholesalerQuery = new Mock<IWholesalerQuery>();
+            mockedWholesalerQuery.Setup(query => query.GetWholesalerStock(It.IsAny<Guid>()))
+                .ReturnsAsync(wholesalerStock);
+
+            var wholesalerService = new WholesalerService(mockedWholesalerQuery.Object, null);
+
+            //Act
+            var result = await wholesalerService.GenerateQuote(quoteRequest);
+
+            //Assert
+            Assert.Equal(expectedPrice, result.Price);
+            Assert.Equal(expectedRawPrice, result.RawPrice);
+            Assert.Equal(expectedDiscout, result.Discount);
+        }
+
+        [Fact]
+        public async Task GenerateQuote_GivenAQuoteRequestWithMoreThan20Items_ShouldReturnAPriceWith20PorcentDiscountFromRawPrice()
+        {
+            //Arrange
+            var requestedItemId = Guid.NewGuid();
+
+            var wholesalerStock = new[] {
+                new StockItem{ItemId = requestedItemId, Quantity = 25, UnitPrice = 1.0f}
+            };
+
+            var expectedRawPrice = 25.0f;
+            var expectedDiscout = 5.0f;
+            var expectedPrice = 20.0f;
+
+            var quoteRequest = new QuoteRequest { WholesalerId = Guid.NewGuid(), RequestItems = new[] { new RequestItem { ItemId = requestedItemId, Quantity = 25 } } };
+
+            var mockedWholesalerQuery = new Mock<IWholesalerQuery>();
+            mockedWholesalerQuery.Setup(query => query.GetWholesalerStock(It.IsAny<Guid>()))
+                .ReturnsAsync(wholesalerStock);
+
+            var wholesalerService = new WholesalerService(mockedWholesalerQuery.Object, null);
+
+            //Act
+            var result = await wholesalerService.GenerateQuote(quoteRequest);
+
+            //Assert
+            Assert.Equal(expectedPrice, result.Price);
+            Assert.Equal(expectedRawPrice, result.RawPrice);
+            Assert.Equal(expectedDiscout, result.Discount);
+        }
+    }
+}
